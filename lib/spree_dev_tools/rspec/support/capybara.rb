e806494b4e6eedaba886965009b57b9dba271194
m.b. <mbajur@users.noreply.github.com>
2024-09-07 17:19:11 +0200

    Update capybara.rb

--- lib/spree_dev_tools/rspec/support/capybara.rb
+++ lib/spree_dev_tools/rspec/support/capybara.rb
@@ -8,8 +8,8 @@ require 'capybara-select-2'
 Capybara.configure do |config|
   config.save_path = ENV['CIRCLE_ARTIFACTS'] if ENV['CIRCLE_ARTIFACTS']
   config.server = :puma
-  config.default_driver = :rack_test
-  config.javascript_driver = :selenium_chrome_headless
+  # config.default_driver = :rack_test
+  # config.javascript_driver = :selenium_chrome_headless
   config.default_max_wait_time = ENV.fetch('CAPYBARA_MAX_WAIT_TIME', 45).to_i
   config.always_include_port = true
   config.match = :smart
